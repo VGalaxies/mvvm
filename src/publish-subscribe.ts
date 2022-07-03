9bf4dd530a547b9a622b12a3118ae2817b4bb0cd
V_Galaxy <1904821183@qq.com>
2022-07-03 14:25:05 +0800

    feat: publish-subscribe once and off

--- src/publish-subscribe.ts
+++ src/publish-subscribe.ts
@@ -1,39 +1,57 @@
-class EventEmitter {
+export class EventEmitter {
   // 缓存列表，存放 event 及 fn
-  private list: object;
-  public on: Function;
-  public emit: Function;
+  private list: any = {};
 
-  constructor(_list: object, _on: Function, _emit: Function) {
-    this.list = _list;
-    this.on = _on;
-    this.emit = _emit;
-  }
+  public on: Function = (event: string, fn: Fn) => {
+    const _this = this;
+    // 如果对象中没有对应的 event 值，也就是说明没有订阅过，就给 event 创建个缓存列表
+    // 如有对象中有相应的 event 值，把 fn 添加到对应 event 的缓存列表里
+    (_this.list[event] || (_this.list[event] = [])).push(fn);
+    return _this;
+  };
+
+  public emit: Function = (event: string, ...args: any) => {
+    const _this = this;
+    // 第一个参数是对应的 event 值
+    const fns = _this.list[event];
+    // 如果缓存列表里没有 fn 就返回 false
+    if (!fns || fns.length === 0) {
+      return false;
+    }
+    // 遍历 event 值对应的缓存列表，依次执行 fn
+    fns.forEach((fn: { apply: (arg0: any, arg1: any) => void }) => {
+      fn.apply(_this, args);
+    });
+    return _this;
+  };
+
+  public off: Function = (event: string, fn: Function) => {
+    const _this = this;
+    let fns = _this.list[event];
+    // 如果缓存列表中没有相应的 fn，返回 false
+    if (!fns || fns.length === 0) {
+      return false;
+    }
+    // 遍历缓存列表，看看传入的 fn 与哪个函数相同，如果相同就直接从缓存列表中删掉即可
+    _this.list[event] = fns.filter((fb: Fn) => {
+      return fb !== fn && fb.fn !== fn;
+    });
+    return _this;
+  };
+
+  public once: Function = (event: string, fn: Function) => {
+    // 先绑定，调用后删除
+    let _this = this;
+    function on() {
+      _this.off(event, on);
+      fn.apply(_this, arguments);
+    }
+    on.fn = fn; // for off
+    _this.on(event, on);
+    return _this;
+  };
 }
 
-export const emitter = new EventEmitter({}, _on, _emit);
-
-// 订阅
-function _on(event: string, fn: Function) {
-  const _this = this;
-  // 如果对象中没有对应的 event 值，也就是说明没有订阅过，就给 event 创建个缓存列表
-  // 如有对象中有相应的 event 值，把 fn 添加到对应 event 的缓存列表里
-  (_this.list[event] || (_this.list[event] = [])).push(fn);
-  return _this;
-}
-
-// 发布
-function _emit(event: string, ...args: any) {
-  const _this = this;
-  // 第一个参数是对应的 event 值
-  const fns = _this.list[event];
-  // 如果缓存列表里没有 fn 就返回 false
-  if (!fns || fns.length === 0) {
-    return false;
-  }
-  // 遍历 event 值对应的缓存列表，依次执行 fn
-  fns.forEach((fn: { apply: (arg0: any, arg1: any) => void }) => {
-    fn.apply(_this, args);
-  });
-  return _this;
+interface Fn extends Function {
+  fn?: Function;
 }
--- test/publish-subscribe.test.ts
+++ test/publish-subscribe.test.ts
@@ -2,27 +2,28 @@ import { it, describe } from "mocha";
 const assert = require("chai").assert;
 const stdout = require("test-console").stdout;
 
-import { emitter } from "../src/publish-subscribe";
+import { EventEmitter } from "../src/publish-subscribe";
 
-describe("publish-subscribe", function () {
-  it("on-emit", function () {
+describe("publish-subscribe-test", () => {
+  function foo(content: string) {
+    console.log(`foo <- ${content}`);
+  }
+
+  function bar(content: string) {
+    console.log(`bar <- ${content}`);
+  }
+
+  function add(x: number, y: number) {
+    console.log(`${x} + ${y} = ${x + y}`);
+  }
+
+  function sub(x: number, y: number) {
+    console.log(`${x} - ${y} = ${x - y}`);
+  }
+
+  it("on-emit", () => {
+    const emitter = new EventEmitter();
     const inspect = stdout.inspect();
-
-    function foo(content: string) {
-      console.log(`foo <- ${content}`);
-    }
-
-    function bar(content: string) {
-      console.log(`bar <- ${content}`);
-    }
-
-    function add(x: number, y: number) {
-      console.log(`${x} + ${y} = ${x + y}`);
-    }
-
-    function sub(x: number, y: number) {
-      console.log(`${x} - ${y} = ${x - y}`);
-    }
 
     emitter.on("article", foo);
     emitter.on("article", bar);
@@ -34,6 +35,7 @@ describe("publish-subscribe", function () {
     emitter.emit("math", 300, 120);
 
     inspect.restore();
+
     let expected: Array<String> = [
       `foo <- hello\n`,
       `bar <- hello\n`,
@@ -42,6 +44,51 @@ describe("publish-subscribe", function () {
       `300 + 120 = 420\n`,
       `300 - 120 = 180\n`,
     ];
+    assert.deepEqual(inspect.output, expected);
+  });
+
+  it("on-off-emit", () => {
+    const emitter = new EventEmitter();
+    const inspect = stdout.inspect();
+
+    emitter.on("article", foo);
+    emitter.on("article", bar);
+    emitter.off("article", foo);
+    emitter.emit("article", "hello");
+
+    inspect.restore();
+
+    let expected: Array<String> = [`bar <- hello\n`];
+    assert.deepEqual(inspect.output, expected);
+  });
+
+  it("on-once-emit", () => {
+    const emitter = new EventEmitter();
+    const inspect = stdout.inspect();
+
+    emitter.on("article", foo);
+    emitter.once("article", bar);
+    emitter.off("article", foo);
+    emitter.emit("article", "hello");
+    emitter.emit("article", "hello");
+
+    inspect.restore();
+
+    let expected: Array<String> = [`bar <- hello\n`];
+    assert.deepEqual(inspect.output, expected);
+  });
+
+  it("once-off-emit", () => {
+    const emitter = new EventEmitter();
+    const inspect = stdout.inspect();
+
+    emitter.once("article", bar);
+    emitter.off("article", bar);
+    emitter.emit("article", "hello");
+
+    inspect.restore();
+
+    let expected: Array<String> = [];
     assert.deepEqual(inspect.output, expected);
   });
 });
